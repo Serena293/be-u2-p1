ec09d0a6a9aee78255f79b53953c39be15cc6e54
SERENA FERRARO <serenaferraro21@gmail.com>
2025-02-07 11:19:46 +0100

    SetUp

---
+++ src/main/java/it/spring/p1/be_u2_p1/BeU2P1Application.java
@@ -0,0 +1,13 @@
+package it.spring.p1.be_u2_p1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeU2P1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeU2P1Application.class, args);
+	}
+
+}
---
+++ src/test/java/it/spring/p1/be_u2_p1/BeU2P1ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.spring.p1.be_u2_p1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeU2P1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
